196690e7424e89989e479cc7071c345d567874aa
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-20 12:44:08 -0800

    Update apiendpoint.py

--- apiendpoint.py
+++ apiendpoint.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import subprocess
 
 # pip install flask
-# set up a virtual environment for your project
+# set up a virtual environment for your project if you don't have one already
 # endpoint to retrieve the current date
 # upon running hit http://127.0.0.1:5000/date
 # you should then see a data JSON response
