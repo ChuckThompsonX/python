16dabef2edb9a571a22acd8df1ce940f8703802c
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-02 05:48:01 -0700

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,33 @@
+a = 7
+b = 2
+print("a + b = ", a + b)  # Addition
+print("a - b = ", a - b)  # Subtraction
+print("a * b = ", a * b)  # Multiplication
+print("a / b = ", a / b)  # Division
+print("a // b = ", a // b)  # Floor Division
+print("a % b = ", a % b)  # Modulus
+print("a ** b = ", a ** b)  # Exponentiation
+
+print("a == b = ", a == b)  # Equal to
+print("a != b = ", a != b)  # Not equal to
+print("a > b = ", a > b)  # Greater than
+print("a < b = ", a < b)  # Less than
+print("a >= b = ", a >= b)  # Greater than or equal to
+print("a <= b = ", a <= b)  # Less than or equal to
+
+print("a is b = ", a is b)  # Identity operator
+print("a is not b = ", a is not b)  # Identity operator
+
+print("not a = ", not a)  # Logical NOT
+print("a and b = ", a and b)  # Logical AND
+print("a or b = ", a or b)  # Logical OR
+
+print("a & b = ", a & b)  # Bitwise AND
+print("a | b = ", a | b)  # Bitwise OR
+print("a ^ b = ", a ^ b)  # Bitwise XOR
+print("a << b = ", a << b)  # Bitwise Left Shift
+print("a >> b = ", a >> b)  # Bitwise Right Shift
+
+fruits = ["apple", "kiwi", "banana", "orange"]
+print("banana" in fruits) # Membership operator
+print("grape" not in fruits) # Membership operator
