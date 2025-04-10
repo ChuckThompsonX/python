3833217134611107d646e36a6cc902d502899dde
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-10 07:18:24 -0700

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,26 @@
+# Data type is set when you assign a value to a variable:
+
+x = "Hello World"						                  #str
+x = 20                                        #int
+x = 20.5								                      #float
+x = 1j								                        #complex
+x = ["apple", "banana", "cherry"]			        #list
+x = ("apple", "banana", "cherry")			        #tuple
+x = range(6)							                    #range
+x = {"name" : "John", "age" : 36}			        #dict
+x = {"apple", "banana", "cherry"}			        #set
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True								                      #bool
+x = b"Hello"							                    #bytes  
+x = bytearray(5)						                  #bytearray
+x = memoryview(bytes(5))					            #memoryview
+x = None								                      #NoneType
+
+'''
+4 built-in data types used to store collections of data: List, Tuple, Set, and Dictionary
+
+List: ordered, changeable, allow duplicates, indexed, []
+Tuple: ordered, UNCHANGEABLE, allow duplicates, indexed, ()
+Set: unordered, unchangeable, NO DUPS, unindexed, {}
+Dictionary: ordered, changeable, no dups, KEY:VALUE PAIRS, unindexed, {}
+'''
