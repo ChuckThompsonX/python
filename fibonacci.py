75f5c40decad56b7015eeeaab42332d651060830
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-10-10 18:55:44 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,8 @@
+# basic example of recursion
+# Fibonacci sequence
+# series of numbers where each number is the sum of the two preceding ones
+# typically starting with 0 and 1
+
 def fib(n):
     if n <= 1:
         return n
